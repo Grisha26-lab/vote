f036b027a7c9d305acc1ec2a4b303e0e2e134279
AvanesovGrigorii <GAvanesov@alfabank.ru>
2022-05-15 13:21:31 +0300

    added getmethod getstats

--- src/main/java/com/avanesov/vote/web/VoteApi.java
+++ src/main/java/com/avanesov/vote/web/VoteApi.java
@@ -2,9 +2,11 @@ package com.avanesov.vote.web;
 
 import com.avanesov.vote.app.domain.Vote;
 import com.avanesov.vote.app.domain.VoteValue;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,31 +21,50 @@ public class VoteApi {
 
 
     @PostMapping(path = "/votes")
-     SaveVoteResponse save ( @RequestBody SaveVoteRequest request){
+    SaveVoteResponse save(@RequestBody SaveVoteRequest request) {
 
         Vote vote = new Vote();
         vote.setUserId(request.getUserId());
         vote.setValue(request.getVoteValue());
-         boolean isAnswer = service.save(vote);
-         SaveVoteResponse saveVoteResponse = new SaveVoteResponse();
-         saveVoteResponse.setSaved(isAnswer);
+        boolean isAnswer = service.save(vote);
+        SaveVoteResponse saveVoteResponse = new SaveVoteResponse();
+        saveVoteResponse.setSaved(isAnswer);
         return saveVoteResponse;
-     }
+    }
+
+
+    @GetMapping("/votes/stats")
+    public GetVoteStatsResponce getStats() {
+        return GetVoteStatsResponce
+                .builder()
+                .build();
+    }
+
 }
 
 @Getter
 @Setter
-class SaveVoteRequest{
+class SaveVoteRequest {
     private UUID userId;
     private VoteValue voteValue;
 
 }
+
 @Getter
 @Setter
-class SaveVoteResponse{
+class SaveVoteResponse {
 
     private boolean isSaved;
 
 }
 
+@Getter
+@Builder
+class GetVoteStatsResponce {
+    @Builder.Default
+    private long totalY = 0;
+    @Builder.Default
+    private long totalN = 0;
+
+}
 
